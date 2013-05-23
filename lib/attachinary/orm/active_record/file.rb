bec33baa5a4d58b17fbd6af121277aa50b7713db
Jerrod <jerrod@reccenter.com>
2013-05-23 16:45:12 -0400

    revert some bad ideas

--- lib/attachinary/orm/active_record/file.rb
+++ lib/attachinary/orm/active_record/file.rb
@@ -1,7 +1,6 @@
 module Attachinary
   class File < ::ActiveRecord::Base
     belongs_to :attachinariable, polymorphic: true
-    attr_accessible :public_id, :version, :width, :height, :format, :resource_type
     include FileMixin
   end
 end
